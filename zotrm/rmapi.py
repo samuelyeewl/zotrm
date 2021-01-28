f8aa8d9a322792ffca6b8d1438001fb1f7d870bf
Samuel <samuelyeewl@gmail.com>
2021-01-27 23:20:36 -0500

    First draft of integration with remarks

---
+++ zotrm/rmapi.py
@@ -0,0 +1,137 @@
+"""
+rmapi.py
+
+Utility functions to call rmapi.
+"""
+import subprocess
+import os
+import re
+
+class RMAPI(object):
+    def __init__(self, rmapi_path, verbose=False):
+        self.rmapi_path = rmapi_path
+        self.verbose = verbose
+
+    def __call__(self, cmd, *args,
+                 stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL):
+        '''
+        Run the rmapi on a given command, returning the status code.
+        '''
+        sub_args = [self.rmapi_path, cmd] + [a for a in args]
+        return subprocess.call(sub_args, stdout=stdout, stderr=stderr)
+
+    def run(self, cmd, *args):
+        '''
+        Run the rmapi on a given command, returning stdout if no error,
+        otherwise returns stderr.
+        '''
+        sub_args = [self.rmapi_path, cmd] + [a for a in args]
+        result = subprocess.run(sub_args, capture_output=True)
+        if result.returncode == 0:
+            return result.stdout
+        else:
+            return result.stderr
+
+    def checkdir(self, dir):
+        '''
+        Check if a given directory exists.
+
+        Args:
+        -----
+        dir : str or list
+            Either a directory string or hierarchy.
+        '''
+        if isinstance(dir, list):
+            dir = '/'.join(dir)
+        result = subprocess.call([self.rmapi_path, "find", dir],
+                                  stdout=subprocess.DEVNULL,
+                                  stderr=subprocess.DEVNULL)
+        return not bool(result)
+
+    def checkfile(self, path):
+        '''
+        Checks if a given path exists, stripping extension.
+        '''
+        path = os.path.splitext(path)[0]
+        result = self("find", path)
+        return not bool(result)
+
+    def mkdir(self, dir):
+        '''
+        Make a directory recursively, if it does not exist.
+
+        Args:
+        -----
+        dir : str or list
+            Either a directory string or hierarchy.
+        '''
+        if isinstance(dir, list):
+            hierarchy = dir
+        else:
+            hierarchy = dir.split('/')
+
+        dirstr = ""
+        direxists = True
+        for folder in hierarchy:
+            dirstr += "/" + folder
+            if len(dirstr) < 2:
+                break
+            # Check if directory exists if parent existed.
+            if direxists:
+                direxists = self.checkdir(dirstr)
+
+            # Create directory if it doesn't exist.
+            if not direxists:
+                status = self("mkdir", dirstr)
+                if status != 0:
+                    raise Exception("Could not create directory "
+                                    + dirstr + " on remarkable.")
+                if verbose:
+                    print("\tCreated directory {:s} on remarkable".format(dirstr))
+
+        return dirstr
+
+    def stat(self, path):
+        '''
+        Run the stat command, returning the results as a dict
+        '''
+        path = os.path.splitext(path)[0]
+        result = subprocess.run([self.rmapi_path, 'stat', path],
+                                capture_output=True)
+
+        if result.returncode != 0:
+            raise Exception("Could not run stat, error: \n{:s}".format(result.stderr.decode('ascii')))
+
+        stat_res_str = result.stdout.split(b'\n')[1].decode('ascii')
+
+        stat_regex = re.compile('[{|\s](\w+):(\S*)(?=[\s|}])')
+        match = stat_regex.findall(stat_res_str)
+        if len(match) < 1:
+            raise Exception("Could not parse result from stat: \n{:s}")
+
+        stat_dict = {}
+        for m in match:
+            stat_dict[m[0]] = m[1]
+
+        return stat_dict
+
+    def get(self, path, dir=None):
+        '''
+        Run the get command, obtaining the desired file into the desired directory.
+
+        Returns
+        -------
+        path to returned zip file
+        '''
+        path = os.path.splitext(path)[0]
+        result = subprocess.run([self.rmapi_path, 'get', path],
+                                capture_output=True, cwd=dir)
+
+        if result.returncode != 0:
+            raise Exception("Could not run get, error: \n{:s}".format(result.stderr.decode('ascii')))
+
+        filename = os.path.basename(path)
+        if dir is None:
+            dir = os.getcwd()
+        return os.path.join(dir, filename + '.zip')
+
--- zotrm/zotrm.py
+++ zotrm/zotrm.py
@@ -5,7 +5,7 @@ Send Zotero papers to ReMarkable tablet.
 
 @authors Samuel Yee, @github/narroo
 """
-import os
+import os, shutil
 import configparser
 import glob
 import re
@@ -13,6 +13,11 @@ import argparse
 import subprocess
 from pyzotero import zotero
 import landscape_pdf
+import rmapi
+import datetime
+import json
+import markdown
+
 
 def read_config():
     '''
@@ -39,8 +44,11 @@ def read_config():
     if 'BASE_DIR' in config['Remarkable']:
         d['rm_base_dir'] = config['Remarkable']['BASE_DIR']
     d['rm_default_dir'] = config['Remarkable']['DEFAULT_DIR']
+    if 'remarks' in config and 'REMARKS_PATH' in config['remarks']:
+        d['remarks_path'] = config['remarks']['REMARKS_PATH']
 
     return d
+
 
 def get_attachment(papers, zot, config):
     '''
@@ -59,6 +67,338 @@ def get_attachment(papers, zot, config):
     '''
 
     return attachments
+
+
+def get_collection_hierarchy(paper, zot, config):
+    '''
+    Get collection hierarchy for a given paper.
+
+    Args:
+    -----
+    paper : dict
+        pyzotero dict
+    zot : zotero.Zotero
+        zotero object
+    config : dict
+        configuration dictionary
+    '''
+    # Get collection(s)
+    collections = paper['data']['collections']
+    if len(collections) < 1:
+        # If not in a collection, use default dir
+        hierarchy = [config['rm_default_dir']]
+    else:
+        hierarchy = []
+        coll = collections[0]
+        coll_info = zot.collection(coll)
+        hierarchy.append(coll_info['data']['name'])
+        # Get full hierarchy
+        while coll_info['data']['parentCollection']:
+            coll = coll_info['data']['parentCollection']
+            coll_info = zot.collection(coll)
+            hierarchy.append(coll_info['data']['name'])
+        if 'rm_base_dir' in config and config['rm_base_dir'] != "/":
+            hierarchy.append(config['rm_base_dir'])
+        hierarchy.reverse()
+
+    return hierarchy
+
+
+def get_pdf_attachments(paper, zot, config, return_zot_dict=False):
+    '''
+    Get a list of PDF attachments for the given paper.
+    '''
+    # Recursively search for attachments
+    queue = [paper]
+    attachments = []
+    while queue:
+        item = queue.pop()
+        # Search for attachments
+        if item['data']['itemType'] != 'attachment':
+            # Note items don't have children to look through
+            if item['data']['itemType'] != 'note':
+                queue += zot.children(item['key'])
+            continue
+        # Get filename
+        if 'filename' in item['data']:
+            filename = item['data']['filename']
+        elif 'path' in item['data']:
+            filename = item['data']['path']
+        else:
+            # No file found in this attachment
+            continue
+
+        # Make sure file is a PDF
+        if '.pdf' not in filename.casefold():
+            continue
+        # Ignore _remarks output
+        elif filename.endswith('_remarks.pdf'):
+            continue
+
+        # Join base file name
+        if filename.startswith('attachments:'):
+            # For linked attachments, we have the whole filename already.
+            filename = filename[12:]
+            if 'zot_attachment_dir' not in config:
+                print("ERR: No attachment directory provided in config file.")
+                continue
+            filename = os.path.join(config['zot_attachment_dir'], filename)
+            if os.path.exists(filename):
+                if return_zot_dict:
+                    attachments.append((filename, item))
+                else:
+                    attachments.append(filename)
+        else:
+            # Without linked attachments, PDF is in some subdirectory.
+            dirname = re.escape(config['zot_storage_dir'])
+            regexfilename = re.escape(filename)
+            regexfilename = os.path.join(dirname, '[A-Z0-9]*/' + regexfilename)
+            r = re.compile(regexfilename)
+            # Match files to list
+            foundfiles = list(filter(r.match, pdflist))
+            if len(foundfiles) < 1 :
+                print("ERR: Cannot find file \"{:s}\" in storage directory \"{:s}\", skipping...".format(filename,dirname))
+                continue
+            else:
+                for f in foundfiles:
+                    if not os.path.exists(f):
+                        continue
+                    if return_zot_dict:
+                        attachments.append((f, item))
+                    else:
+                        attachments.append(f)
+
+    return attachments
+
+
+def add_md_to_paper(paper, md_file, zot, verbose=False):
+    '''
+    Add a markdown file as a zotero note to a paper.
+
+    Returns
+    -------
+    zotero response
+    '''
+    # Read in file
+    if verbose:
+        print("Reading in .md file...")
+    with open(md_file, 'r') as f:
+        md_str = markdown.markdown(f.read())
+    md_str = '<p id="title"><strong>Extracted highlights</strong></p>\n' + md_str
+
+    # Create note
+    if verbose:
+        print("Creating note...")
+    note = zot.item_template('note')
+    note['note'] = md_str
+    note['parentItem'] = paper['key']
+    if verbose:
+        print("Uploading note...")
+    res = zot.create_items([note])
+
+    if '0' not in res['success']:
+        raise Exception("Failed to upload note to Zotero", res)
+
+    return res
+
+def add_pdf_to_paper(attachment, pdf_file, zot, config, verbose=False):
+    '''
+    Add an annotated PDF to the paper
+    '''
+    # We follow the link mode of the original attachment
+    link_mode = attachment['data']['linkMode']
+    if link_mode == "imported_file":
+        # For imported files, we simply upload the file directly
+        res = zot.attachment_simple(pdf_file, attachment['data']['parentItem'])
+    elif link_mode == "linked_file":
+        # For linked files, we create the item directly
+        if verbose:
+            print("Creating new attachment...")
+        new_attachment = zot.item_template('attachment', 'linked_file')
+        new_attachment['title'] = os.path.basename(pdf_file)
+        new_attachment['parentItem'] = attachment['data']['parentItem']
+        new_attachment['contentType'] = 'application/pdf'
+
+        # Get attachment location
+        attachment_fn = attachment['data']['path']
+        if not attachment_fn.startswith('attachments:'):
+            raise Exception("Invalid filename for linked attachment: {:s}".format(attachment_fn))
+        attachment_fn = attachment_fn[12:]
+        attachment_dir, basename = os.path.split(attachment_fn)
+        # Get full attachment directory
+        attachment_dir = os.path.join(config['zot_attachment_dir'], attachment_dir)
+
+        # Move file to appropriate destination
+        out_pdf_file = os.path.join(attachment_dir, basename.replace('.pdf', ' _remarks.pdf'))
+        if verbose:
+            print("Moving pdf file to {:s}".format(out_pdf_file))
+        shutil.move(pdf_file, out_pdf_file)
+
+        # Add attachment path to metadata
+        new_attachment['path'] = attachment['data']['path'].replace('.pdf', ' _remarks.pdf')
+
+        # Upload attachment
+        if verbose:
+            print("Uploading attachment...")
+        res = zot.create_items([new_attachment], attachment['data']['parentItem'])
+    else:
+        raise Exception("Invalid linkMode {:s}".format(link_mode))
+
+    if '0' not in res['success']:
+        raise Exception("Failed to upload pdf to Zotero", res)
+
+    return res
+
+def extract_remarks(path, rmapi, config, outdir='./', metadata=None,
+                    targets=['md', 'pdf'], verbose=False):
+    '''
+    Use the remarks script to extract an annotated file from remarkable
+
+    Note: Uses the modified remarks script.
+    '''
+    # First, get the file off reMarkable
+    dir = os.path.expanduser('~/.zotrm/')
+    basename = os.path.splitext(os.path.basename(path))[0]
+    outzip = rmapi.get(path, dir=dir)
+    if metadata is None:
+        metadata = rmapi.stat(path)
+
+    # Create temporary dir
+    temp_dir = os.path.join(dir, basename + '/')
+    # Overwrite existing comments if necessary
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+    os.mkdir(temp_dir)
+    if verbose:
+        print("Making directory {:s}".format(temp_dir))
+
+    # Unzip
+    if verbose:
+        print("Unzipping results...")
+
+    res = subprocess.run(['unzip', outzip, '-d', temp_dir],
+                         capture_output=True)
+    if res.returncode != 0:
+        raise Exception("Could not unzip file {:s}".format(outzip))
+
+    # Remarks requires a metadata file
+    metadata_fn = os.path.join(temp_dir, metadata['ID'] + '.metadata')
+    min_metadata = {
+        "type": metadata['Type'],
+        "path": metadata['ID'],
+        "visibleName": metadata['VissibleName'],
+        "parent": ""
+    }
+    if verbose:
+        print("Creating metadata file...")
+    with open(metadata_fn, 'w') as f:
+        f.write(json.dumps(min_metadata))
+
+    # Create output directory for annotations
+    annotate_dir = os.path.join(temp_dir, 'annotated/')
+    os.mkdir(annotate_dir)
+
+    # Run remarks
+    remarks_cmd = "cd {:s} && ".format(config['remarks_path'])
+    remarks_cmd += "python -m remarks "
+    remarks_cmd += "'{:s}' '{:s}' ".format(temp_dir, annotate_dir)
+    remarks_cmd += "--targets " + " ".join(targets) + " "
+    if "md" in targets:
+        remarks_cmd += "--combined_md "
+    if "pdf" in targets:
+        remarks_cmd += "--combined_pdf "
+    if verbose:
+        print("Running remarks to extract annotations...: \n{:s}".format(remarks_cmd))
+    res = subprocess.run(remarks_cmd, shell=True, capture_output=True)
+
+    if res.returncode != 0:
+        raise Exception("Could not run remarks, error {:s}".format(result.stderr))
+
+    # Move files to output dir
+    if verbose:
+        print("Moving file to output directory...")
+    out_fns = []
+    remarks_basename = min_metadata['visibleName']
+    if "md" in targets:
+        md_fn = os.path.join(annotate_dir, remarks_basename + '.md')
+        out_md_fn = os.path.join(outdir, basename + '.md')
+        shutil.move(md_fn, out_md_fn)
+        out_fns.append(out_md_fn)
+    if "pdf" in targets:
+        pdf_fn = os.path.join(annotate_dir, remarks_basename + ' _remarks.pdf')
+        out_pdf_fn = os.path.join(outdir, basename + ' _remarks.pdf')
+        shutil.move(pdf_fn, out_pdf_fn)
+        out_fns.append(out_pdf_fn)
+
+    return out_fns
+
+
+def backsync_papers(zot, rmapi, config, verbose=False):
+    '''
+    Backsync papers onto computer.
+    '''
+    # Get list of papers from Zotero that are on the remarkable
+    z = zot.top(tag=config['zot_replace_tag'])
+    if verbose:
+        print("Checking {:d} files on reMarkable".format(len(z)))
+
+    for paper in z:
+        # Get list of attachments
+        attachments = get_pdf_attachments(paper, zot, config, return_zot_dict=True)
+        rm_hierarchy = get_collection_hierarchy(paper, zot, config)
+
+        for (attachment, zot_attach) in attachments:
+            # Check to see if file is on remarkable
+            basename = os.path.basename(attachment)
+            # Full path on remarkable
+            rm_path = "/".join(rm_hierarchy) + "/" + basename
+            on_rm = rmapi.checkfile(rm_path)
+            if verbose and on_rm:
+                print("Found {:s} on reMarkable".format(rm_path))
+            elif not on_rm:
+                print("Could not find {:s} on reMarkable, skipping...".format(rm_path))
+                continue
+
+            # Check last modification date on reMarkable
+            attachment_metadata = rmapi.stat(rm_path)
+            last_modified_str = attachment_metadata['ModifiedClient']
+            # The UTC+0 timezone is sometimes denoted as Z, but this is
+            # not recognized by datetime:
+            # https://discuss.python.org/t/parse-z-timezone-suffix-in-datetime/2220/14
+            last_modified_str = last_modified_str.replace('Z', '+00:00')
+            rm_last_modified = datetime.datetime.fromisoformat(last_modified_str)
+
+            # Check to see if annotated file exists in zotero
+            zot_remarks_attachment = attachment.replace('.pdf', ' _remarks.pdf')
+            if os.path.exists(zot_remarks_attachment):
+                # Check last modified date
+                remarks_last_modified = datetime.datetime.utcfromtimestamp(
+                    os.path.getmtime(zot_remarks_attachment))
+                # Set UTC timezone for comparison
+                remarks_last_modified = remarks_last_modified.replace(
+                    tzinfo=datetime.timezone.utc)
+                # If file on computer is newer than on remarkable, skip this file.
+                if remarks_last_modified > rm_last_modified:
+                    if verbose:
+                        print("File on reMarkable is older than computer, skipping...")
+                    continue
+
+            # If there is no current annotated file or the file on the computer
+            # is outdated, then we should get the annotated file.
+
+            # Use remarks to extract PDF and MD
+            annotated_files = extract_remarks(rm_path, rmapi, config,
+                                              metadata=attachment_metadata,
+                                              verbose=verbose, targets=['md', 'pdf'])
+
+            # Add extracted files to zotero notes
+            if verbose:
+                print("Adding extracted highlights to zotero...")
+            md_res = add_md_to_paper(paper, annotated_files[0], zot,
+                                     verbose=verbose)
+            pdf_res = add_pdf_to_paper(zot_attach, annotated_files[1], zot, config,
+                                       verbose=verbose)
+            return
 
 
 def main(verbose=False, landscape=False):
@@ -84,54 +424,7 @@ def main(verbose=False, landscape=False):
             print("Preparing paper {:s}".format(paper['data']['title']))
 
         # Find PDF
-        attachments = []
-
-        # Recursively search for attachments
-        queue = [paper]
-        while queue:
-            item = queue.pop()
-            if item['data']['itemType'] != 'attachment':
-                # Note items don't have children to look through
-                if item['data']['itemType'] != 'note':
-                    queue += zot.children(item['key'])
-                continue
-            # Get filename
-            if 'filename' in item['data']:
-                filename = item['data']['filename']
-            elif 'path' in item['data']:
-                filename = item['data']['path']
-            else:
-                # No file found in this attachment
-                continue
-
-            # Make sure file is a PDF
-            if '.pdf' not in filename.casefold():
-                continue
-            # Join base file name
-            if filename.startswith('attachments:'):
-                # For linked attachments, we have the whole filename already.
-                filename = filename[12:]
-                if 'zot_attachment_dir' not in config:
-                    print("ERR: No attachment directory provided in config file.")
-                    continue
-                filename = os.path.join(config['zot_attachment_dir'], filename)
-                if os.path.exists(filename):
-                    attachments.append(filename)
-            else:
-                # Without linked attachments, PDF is in some subdirectory.
-                dirname = re.escape(config['zot_storage_dir'])
-                regexfilename = re.escape(filename)
-                regexfilename = os.path.join(dirname, '[A-Z0-9]*/' + regexfilename)
-                r = re.compile(regexfilename)
-                # Match files to list
-                foundfiles = list(filter(r.match, pdflist))
-                if len(foundfiles) < 1 :
-                    print("ERR: Cannot find file \"{:s}\" in storage directory \"{:s}\", skipping...".format(filename,dirname))
-                    continue
-                else:
-                    for f in foundfiles:
-                        if os.path.exists(f):
-                            attachments.append(f)
+        attachments = get_pdf_attachments(paper, zot, config)
 
         # If no attachments were found, skip the upload
         if not attachments:
